e523d986c4f0292318e1139e047bc8e58927f53a
Safia Hirsi <safa.hirsi@gmail.com>
2020-05-16 23:29:58 -0500

    added post-list component

--- src/app/components/posts/post-list/post-list.component.ts
+++ src/app/components/posts/post-list/post-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-
+posts = [{title: 'Post 1', content: 'Content 1'}, {title: 'Post 2', content:}]
   constructor() { }
 
   ngOnInit(): void {
