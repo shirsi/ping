0b5731c412daac4942aa07cad541594e7b83ac07
Safia Hirsi <safa.hirsi@gmail.com>
2020-05-18 23:30:22 -0500

    create post service

--- src/app/components/posts/post-list/post-list.component.ts
+++ src/app/components/posts/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -9,7 +10,7 @@ import { Post } from '../post.model';
 export class PostListComponent implements OnInit {
   @Input() posts: Post[] = [];
 
-  constructor() {}
+  constructor(public postsService: PostsService) {}
 
   ngOnInit(): void {}
 }
